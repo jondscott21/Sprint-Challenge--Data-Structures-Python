4268278a9cf40c0d763572a05b3b4359d52af43f
Jonathan Scott <jonathan.scott@gmail.com>
2019-12-20 08:15:36 -0800

    ring buffer dll working

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,21 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            self.storage.add_to_head(item)
+            self.current = self.storage.tail
+        else:
+            self.current.value = item
+            self.current = self.current.prev
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
+        cur = self.storage.tail
+        while cur is not self.storage.head:
+            list_buffer_contents.append(cur.value)
+            cur = cur.prev
+        list_buffer_contents.append(self.storage.head.value)
 
         return list_buffer_contents
 
